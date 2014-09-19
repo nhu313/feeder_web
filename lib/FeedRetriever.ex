4fd59bf73abcb6b19fe249196671cf2158d31351
Nhu Nguyen <nhu313@gmail.com>
2014-09-18 20:01:50 -0700

    has feeds

---
+++ lib/FeedRetriever.ex
@@ -0,0 +1,30 @@
+defmodule FeedRetriever do
+  def feeds do
+    pmap(Feeder.Feed.Queries.all, &retrieve_content(&1))
+      |> Enum.concat
+  end
+
+  def pmap(collection, fun) do
+    me = self
+    collection
+    |> Enum.map(fn (elem) -> spawn_link fn -> (send me, { self, fun.(elem) }) end end)
+    |> Enum.map(fn (pid) -> receive do { ^pid, result } -> result end end)
+  end
+
+  def retrieve_content(feed) do
+    IO.puts "parsing #{feed.url}"
+    HTTPotion.start
+    response = HTTPotion.get feed.url
+    RssParser.parse(response.body, feed.type)
+      |> Enum.map(&strip_html_tags(&1))
+      |> Enum.map(&shorten_body(&1))
+  end
+
+  def shorten_body(item) do
+    %{item | body: String.slice(item.body, 0, 200)}
+  end
+
+  def strip_html_tags(node) do
+    %{node | body: String.replace(node.body, ~r/<[^>]*>/, "")}
+  end
+end
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,29 +2,8 @@ defmodule FeederWeb.PageController do
   use Phoenix.Controller
 
   def feeds(conn, _params) do
-    feeds = Enum.map(Feeder.Feed.Queries.all, &retrieve_content(&1))
-            |> Enum.concat
-
-    IO.puts length(feeds)
-    render conn, "feeds", feeds: feeds
+    render conn, "feeds", feeds: FeedRetriever.feeds
   end
-
-  def retrieve_content(feed) do
-    HTTPotion.start
-    response = HTTPotion.get feed.url
-    RssParser.parse(response.body, feed.type)
-      |> Enum.map(&strip_html_tags(&1))
-      |> Enum.map(&shorten_body(&1))
-  end
-
-  def shorten_body(item) do
-    %{item | body: String.slice(item.body, 0, 200)}
-  end
-
-  def strip_html_tags(node) do
-    %{node | body: String.replace(node.body, ~r/<[^>]*>/, "")}
-  end
-
 
   def index(conn, _params) do
     render conn, "index", feeds: Feeder.Feed.Queries.all
